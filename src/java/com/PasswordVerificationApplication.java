af6a624abde865bb6ff6adbc35da901913fc7207
Shashank S <131436859+Shashank8602@users.noreply.github.com>
2025-03-12 13:16:44 +0530

    Add files via upload

---
+++ src/java/com/PasswordVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.password_verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordVerificationApplication.class, args);
+	}
+
+}
